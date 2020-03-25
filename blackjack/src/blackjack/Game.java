a37f09ee95ad975a203d8805ce5a517633a91d80
luke <lukesosbourne@gmail.com>
2020-03-25 13:17:41 +0000

    completed the blackjack game exercise

---
+++ blackjack/src/blackjack/Game.java
@@ -0,0 +1,25 @@
+package blackjack;
+
+public class Game {
+
+	public int newHand(int num1, int num2) {
+
+		if (num1 > 0 && num2 > 0) {
+			if (num1 > 21 && num2 > 21) {
+				return 0;
+			} else if (num1 > 21) {
+				return num2;
+			} else if (num2 > 21) {
+				return num1;
+			} else if (num1 > num2) {
+				return num1;
+			} else {
+				return num2;
+			}
+		} else {
+			return 0;
+		}
+
+	}
+
+}
---
+++ blackjack/src/blackjack/Runner.java
@@ -0,0 +1,17 @@
+package blackjack;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 20;
+		int num2 = 20;
+
+		Game newGo = new Game();
+
+		System.out.println(newGo.newHand(num1, num2));
+
+	}
+
+}
